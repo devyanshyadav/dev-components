66e1dbb6658d02214fb4c828c2e5ac91b27621ea
Avegya - Web Development Services <139138920+devyanshyadav@users.noreply.github.com>
2024-06-21 01:17:56 +0530

    replace:params-path

--- src/components/pagination.tsx
+++ src/components/pagination.tsx
@@ -1,5 +1,5 @@
 "use client";
-import {usePathname } from "next/navigation";
+import { usePathname } from "next/navigation";
 import React from "react";
 import ComponentDataArray from "../../public/component-data-array";
 import CmpUrl from "@/utils/cmp-url";
@@ -11,21 +11,19 @@ const Pagination = () => {
 
   const getCurrentIndex = () => {
     return ComponentDataArray.findIndex(
-      (item) => `/components/${CmpUrl(item.name)}` === path
+      (item) => `/components/${CmpUrl(item?.name)}` === path
     );
   };
 
   const getData = () => {
     const currentIndex = getCurrentIndex();
-
     if (currentIndex === -1) {
       console.log("Component not found");
+      return { current: null, prev: null, next: null };
     }
-
     const prevIndex =
       currentIndex > 0 ? currentIndex - 1 : ComponentDataArray.length - 1;
     const nextIndex = (currentIndex + 1) % ComponentDataArray.length;
-
     return {
       current: ComponentDataArray[currentIndex],
       prev: ComponentDataArray[prevIndex],
@@ -35,22 +33,30 @@ const Pagination = () => {
 
   const { current, prev, next } = getData();
 
+  if (!current) {
+    return null; // or some fallback UI
+  }
+
   return (
     <div className="w-full flex items-center justify-between">
-      <Link
-        href={`/components/${CmpUrl(current.name)}`}
-        className="hover:bg-accent/30 p-1 justify-center text-sm rounded-full px-3 text-accent flex items-center gap-1"
-      >
-        <FaChevronLeft />
-        {prev.name}
-      </Link>
-      <Link
-        href={`/components/${CmpUrl(next.name)}`}
-        className="hover:bg-accent/30 p-1 justify-center text-sm rounded-full px-3 text-accent flex items-center gap-1"
-      >
-        {next.name}
-        <FaChevronRight />
-      </Link>
+      {prev && (
+        <Link
+          href={`/components/${CmpUrl(prev.name)}`}
+          className="hover:bg-accent/30 p-1 justify-center text-sm rounded-full px-3 text-accent flex items-center gap-1"
+        >
+          <FaChevronLeft />
+          {prev.name}
+        </Link>
+      )}
+      {next && (
+        <Link
+          href={`/components/${CmpUrl(next.name)}`}
+          className="hover:bg-accent/30 p-1 justify-center text-sm rounded-full px-3 text-accent flex items-center gap-1"
+        >
+          {next.name}
+          <FaChevronRight />
+        </Link>
+      )}
     </div>
   );
 };
