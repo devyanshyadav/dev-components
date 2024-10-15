4b7b20932c58c4d57383ad85d9fa580191fde85c
Avegya - Web Development Services <developerdevyansh@gmail.com>
2024-10-15 13:18:49 +0530

    updated light label

--- src/utils/zust-store.ts
+++ src/utils/zust-store.ts
@@ -15,10 +15,10 @@ interface ZustState {
 
 const useZustStore = create<ZustState>()((set) => ({
   ACCENT: { hex: "#06b6d4", rgb: { r: 6, g: 182, b: 212 } },
-  LIGHT: { hex: "#06b6d4", rgb: { r: 245, g: 248, b: 255} },
+  LIGHT: { hex: "#F5F8FF", rgb: { r: 245, g: 248, b: 255} },
   DARK: { hex: "#1f2937", rgb: { r: 31, g: 41, b: 55 } },
   AccentLabel: "[#06b6d4]",
-  LightLabel: "[#06b6d4]",
+  LightLabel: "[#F5F8FF]",
   DarkLabel: "[#1f2937]",
 }));
 
