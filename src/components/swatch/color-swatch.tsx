94ede00309dbbdcc2501524482a470da7920605f
Avegya - Web Development Services <developerdevyansh@gmail.com>
2024-10-15 13:23:59 +0530

    updated light label

--- src/components/swatch/color-swatch.tsx
+++ src/components/swatch/color-swatch.tsx
@@ -36,10 +36,10 @@ const ColorSwatch = () => {
   const resetSwatch = () => {
     useZustStore.setState({
       ACCENT: { hex: "#06b6d4", rgb: { r: 6, g: 182, b: 212 } },
-      LIGHT: { hex: "#06b6d4", rgb: { r: 245, g: 248, b: 255 } },
+      LIGHT: { hex: "#F5F8FF", rgb: { r: 245, g: 248, b: 255 } },
       DARK: { hex: "#1f2937", rgb: { r: 31, g: 41, b: 55 } },
       AccentLabel: "[#06b6d4]",
-      LightLabel: "[#06b6d4]",
+      LightLabel: "[#F5F8FF]",
       DarkLabel: "[#1f2937]",
     });
   };
