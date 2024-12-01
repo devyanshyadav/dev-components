e3af2e1719e6446b00bb9ab3263553c64dbaaadf
Devyansh | localhost:3000 <developerdevyansh@gmail.com>
2024-12-02 00:04:42 +0530

    fixed dev-drawer-v2

--- public/dev-components/drawers/(dev-drawer-v2)/dev-drawer-v2.tsx
+++ public/dev-components/drawers/(dev-drawer-v2)/dev-drawer-v2.tsx
@@ -45,7 +45,7 @@ export default function DevDrawerV2({
           } z-50`}
         />
         <Drawer.Content
-          className={`z-50 bg-LIGHT shadow-[3px_200px_0px_1px_rgba(0,0,0,0)] border-ACCENT/50 dark:bg-DARK fixed flex flex-col  ${
+          className={`z-50 bg-LIGHT shadow-[0px_200px_0px_0px] dark:shadow-DARK shadow-LIGHT border-ACCENT/50 dark:bg-DARK fixed flex flex-col  ${
             getPositionClasses[position]
           } ${
             accommodate &&
